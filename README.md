914e11ca20eabc673d15ca8afbce8a75722c8805
elssar <elssar@altrawcode.com>
2016-03-07 09:48:34 +0530

    Initial pass at the hook

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 [![Build Status][travis-image]][travis-url] [![Coverage Status][coveralls-image]][coveralls-url]
 
 Sails hook to ensure that only requests made from authorized hosts/IP addresses are allowed. Inspired by the
-[`allowed_hosts`](Sails hook to ensure that only requests made from authorized hosts/IP addresses are allowed)
+[`ALLOWED_HOSTS`](https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts)
 setting in Django.
 
 [travis-image]: https://travis-ci.org/elssar/sails-hook-allowed-hosts.svg?branch=master
--- index.js
+++ index.js
@@ -1,1 +1,78 @@
-module.exports = {};
+module.exports = function (sails) {
+  var util = require('sails-util'),
+    /**
+     * Definition for sails-hook-allowed-hosts
+     * Conforms to http://sailsjs.org/documentation/concepts/extending-sails/hooks/hook-specification
+     *
+     * This hook takes in an array of strings and regular-expressions that whitelist a hostname, and matches
+     * the host header of all incoming requests against that array. If the hostname is whitelisted,
+     * lets the request go throug, else returns an error.
+     *
+     * Inspired by the ALLOWED_HOSTS (https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts)
+     * setting in Django
+     *
+     * @type {Object}
+     */
+    allowedHostsHookDef = {
+      /**
+       * Default configuration for the hook
+       *
+       * @type {Object}
+       */
+      deafults: {
+        __configKey__: {
+          allowedHosts: [/.*/],
+          skipEnvs: ['test', 'development'],
+          errorResponse: function (res) {
+            sails.log.error('sails-hook-allowed-hosts :: Invalid host header');
+            return res.badRequest();
+          }
+        }
+      },
+      /**
+       * Inialize the hook
+       *
+       * @param  {Function} cb
+       */
+      initialize: function (cb) {
+        cb = util.optional(cb);
+
+        sails.log.verbose('sails-hook-allowed-hosts loaded successfully');
+        return cb();
+      },
+      routes: {
+        before: {
+          /**
+           * Before a request is passed to the router, check whether the host header matches a host
+           * or host pattern set in allowedHosts
+           * If there is a match, allow the request to go through, else return an error
+           *
+           * @param  {Object}   req  Sails request object
+           * @param  {Object}   res  Sails response object
+           * @param  {Function} next
+           */
+          'all /*': function (req, res, next) {
+            var self = allowedHostsHookDef,
+              config = sails.config[self.configKey],
+              pass;
+
+            if (util.includes(config.skipEnvs, sails.config.environment)) {
+              return next();
+            }
+
+            pass = !(util.isUndefined(util.find(config.allowedHosts, function (aH) {
+              return req.host.match(aH);
+            })));
+
+            if (pass) {
+              return next();
+            }
+
+            return config.errorResponse(res);
+          }
+        }
+      }
+    };
+
+  return allowedHostsHookDef;
+};
